24a5c42f6e4996c26de2df33351a52c14538ef5f
eduardonicioli <111098632+eduardonicioli@users.noreply.github.com>
2025-07-16 14:12:57 -0300

    update: db_config
    remoção dos dados para conectar no serv00

--- public/includes/db_config.php
+++ public/includes/db_config.php
@@ -3,10 +3,10 @@
 
 // Substitua 'SEU_HOST_BD', 'SEU_USUARIO_BD', 'SUA_SENHA_BD', 'SEU_NOME_BD'
 // pelas informações que o serv00.net te fornece no painel de controle do MySQL.
-define('DB_HOST', '-----------'); // Geralmente é 'localhost', mas verifique no seu painel serv00.net
-define('DB_USER', '-------');   // EX: 'uXXXXX_usuario_bd'
-define('DB_PASS', '--------');     // EX: 'SenhaForte123'
-define('DB_NAME', '-------');     // EX: 'uXXXXX_nome_do_banco'
+define('DB_HOST', 'xxxxxxxxx'); // Geralmente é 'localhost', mas verifique no seu painel serv00.net
+define('DB_USER', 'xxxxxxxxx');   // EX: 'uXXXXX_usuario_bd'
+define('DB_PASS', 'xxxxxxxxx');     // EX: 'SenhaForte123'
+define('DB_NAME', 'xxxxxxxxx');     // EX: 'uXXXXX_nome_do_banco'
 
 // Este arquivo DEVE estar FORA da pasta public_html para segurança!
 // Ele NÃO deve ser acessível diretamente pelo navegador.
